0efbdb2a1047bb1816d445221436df25f85d73c8
ephrem <ephremmamo555@gmail.com>
2024-10-14 15:42:28 +0300

    add left side bar and mobile nav

--- wealthnest/app/(root)/layout.tsx
+++ wealthnest/app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import MobileNav from '@/components/MobileNav';
+import Sidebar from '@/components/Sidebar';
+import { log } from 'console';
+import Image from 'next/image';
 import React from 'react';
 
 function Layout({
@@ -5,10 +9,19 @@ function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Ephrem', lastName: 'Mamo' };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
